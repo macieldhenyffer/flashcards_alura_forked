3b9e78ecd971edec7838f7953148f9f43dd89335
macieldhenyffer <dhenyffer.maciel@escola.pr.gov.br>
2025-06-10 23:32:22 +0000

    dhe

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,25 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Cuidado com a saude animal ',
+    'Quais são os principais cuidados necessários para mantér um cavalo bonito e forte?',
+    'Ofereça ração específica para equinos de boa qualidade, Sempre forneça água limpa e fresca, Se o cavalo tem alguma necessidade especial, como ser mais ativo ou estar em treinamento, você pode precisar ajustar a quantidade e tipo de alimentos.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+   '  ',
+    'Você costuma levar seu cavalo a fazer exercícios físicos?',
+    'Como o Mangalarga Marchador é conhecido por sua marcha, é importante incentivar o cavalo a praticar sua marcha adequada. O treino pode incluir caminhadas e trotes, intercalados com a marcha, para reforçar o movimento.'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Você sabe como funciona pelagem e higiene de um  cavalo?',
+    'Escovar o cavalo regularmente remove a sujeira, o pelo solto e melhora a circulação sanguínea. Você pode dar banhos periódicos, especialmente se o cavalo estiver muito suado após exercícios intensos. Cuidado para não dar banhos com água muito fria, pois isso pode ser um choque para o animal',
+
+
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
+    'Como se dis?',
     'Oi em ingles é HI (RAI)'
 )
