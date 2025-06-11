6a34cee40a4553ba61a1fa6eb5719c3b2908ce28
macieldhenyffer <dhenyffer.maciel@escola.pr.gov.br>
2025-06-11 01:38:52 +0000

    dheny do rafa

--- perguntas.js
+++ perguntas.js
@@ -1,25 +1,25 @@
 criaCartao(
-    'Cuidado com a saude animal ',
-    'Quais são os principais cuidados necessários para mantér um cavalo bonito e forte?',
+    ' MM ',
+    'Quais são os principais cuidados necessários para manter um cavalo bonito e forte?',
     'Ofereça ração específica para equinos de boa qualidade, Sempre forneça água limpa e fresca, Se o cavalo tem alguma necessidade especial, como ser mais ativo ou estar em treinamento, você pode precisar ajustar a quantidade e tipo de alimentos.'
 )
 
 criaCartao(
-   '  ',
+   ' MM ',
     'Você costuma levar seu cavalo a fazer exercícios físicos?',
     'Como o Mangalarga Marchador é conhecido por sua marcha, é importante incentivar o cavalo a praticar sua marcha adequada. O treino pode incluir caminhadas e trotes, intercalados com a marcha, para reforçar o movimento.'
 )
 
 criaCartao(
-    'Programação',
-    'Você sabe como funciona pelagem e higiene de um  cavalo?',
+    'MM',
+    'Você sabe como funciona pelagem e higiene de um cavalo?',
     'Escovar o cavalo regularmente remove a sujeira, o pelo solto e melhora a circulação sanguínea. Você pode dar banhos periódicos, especialmente se o cavalo estiver muito suado após exercícios intensos. Cuidado para não dar banhos com água muito fria, pois isso pode ser um choque para o animal',
 
 
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se dis?',
-    'Oi em ingles é HI (RAI)'
+    'MM',
+    'Você sabe a origem do Mangalarga Marchador?',
+    'A origem do Mangalarga Marchador está profundamente ligada à história e à formação da cavalaria brasileira. A raça foi desenvolvida no Brasil, especialmente no Estado de Minas Gerais, a partir de um processo de cruzamento entre cavalos de origem europeia, principalmente os lusitanos e andaluzes, e cavalos nativos do Brasil.'
 )
