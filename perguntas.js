5d191f79fc53fce42dbbc9d7abacbd44359404ca
macieldhenyffer <dhenyffer.maciel@escola.pr.gov.br>
2025-06-17 23:28:55 +0000

    dheny

--- perguntas.js
+++ perguntas.js
@@ -23,3 +23,43 @@ criaCartao(
     'Você sabe a origem do Mangalarga Marchador?',
     'A origem do Mangalarga Marchador está profundamente ligada à história e à formação da cavalaria brasileira. A raça foi desenvolvida no Brasil, especialmente no Estado de Minas Gerais, a partir de um processo de cruzamento entre cavalos de origem europeia, principalmente os lusitanos e andaluzes, e cavalos nativos do Brasil.'
 )
+criaCartao(
+   ' MM ',
+    'Qual é a principal marcha do Cavalo Mangalarga Marchador?',
+    'A principal marcha do Cavalo Mangalarga Marchador é a marcha picada, também conhecida como marcha',
+) 
+criaCartao(
+    'MM',
+    'Qual é o temperamento do Cavalo Mangalarga Marchador?',
+    'Energia moderada – Embora sejam ativos, os Mangalarga Marchador não costumam ser excessivamente agitados. Eles têm uma energia equilibrada, o que é ótimo para cavalgadas longas e atividades em que o cavaleiro precisa de um cavalo que saiba controlar sua força',
+)
+criaCartao(
+    'MM',
+    'Qual é o Espaço Adequado?',
+    'Pasto ou piquete: O cavalo precisa de espaço suficiente para se mover. O ideal é ter uma área de pasto ou um piquete bem cercado para que ele possa caminhar e pastar. Estábulo: Embora o cavalo precise de liberdade para se movimentar, ele também vai precisar de um local seguro para descansar. O estábulo deve ser bem ventilado, com cama de feno ou serragem para conforto',
+)
+criaCartao(
+    'MM',
+    'Quais são os equipamento básico?',
+    'Sela: Escolher uma sela confortável para o cavalo, que não machuque nem no dorso nem nos ombros. Para o Mangalarga Marchador, é importante escolher uma sela que permita a movimentação das marchas sem restrições.Rédeas e cabresto: Usados para conduzir o cavalo. O cabresto deve ser confortável e não apertar muito.',
+)
+criaCartao(
+    'MM',
+    'Quais são os custos?',
+    'Custos com alimentação e cuidados: Além de aquisição inicial, o custo de manter um cavalo envolve alimentação, cuidados veterinários, ferraduras e equipamentos.Manutenção do espaço: O local onde o cavalo ficará precisa estar bem mantido, o que pode gerar custos com cercas, reparos e limpeza.',
+)
+criaCartao(
+   ' MM ',
+    'Qual é a diferença entre a marcha picada e a marcha de pega?',
+    'Muitas pessoas querem entender as diferenças entre as duas principais marchas do Mangalarga Marchador, já que a picada é mais suave e confortável para passeios longos, enquanto a marcha de pega é mais rápida e intensa, usada em competições.',
+) 
+criaCartao(
+   ' MM ',
+    ' O Mangalarga Marchador é bom para iniciantes?',
+    'O Mangalarga Marchador é uma excelente escolha para iniciantes, pois tem um temperamento equilibrado, é gentil e se adapta bem aos cavalos iniciantes. No entanto, é sempre importante ter alguém experiente por perto, especialmente para aprender a montar e cuidar do animal adequadamente.',
+) 
+criaCartao(
+   ' MM ',
+    'O que fazer quando meu Mangalarga Marchador está muito agitado?',
+    'Muitas pessoas perguntam como lidar com cavalos agitados, especialmente se o animal não está se comportando durante a montaria ou o treinamento. A resposta geralmente envolve verificar a saúde do animal, garantir que ele tenha tempo suficiente de descanso e exercitar o cavalo regularmente para liberar energia.',
+) 
